ada409fc29b6b50e91f8a83b185b644dbb708824
maulana <maulanafahrul2004@gmail.com>
2023-07-04 14:29:01 +0900

    [FEATURE] add getid on farmer usecase and handler and make some validate for that

--- handler/farmers_handler.go
+++ handler/farmers_handler.go
@@ -1,7 +1,12 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"live-code-farmer-ii/apperror"
 	"live-code-farmer-ii/usecase"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +18,53 @@ type farmersHandlerImpl struct {
 	frmsUsecase usecase.FarmersUsecase
 }
 
+func (frmsHandler *farmersHandlerImpl) GetFarmerByIdHandler(ctx *gin.Context) {
+	idText := ctx.Param("id")
+	if idText == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success":      false,
+			"errorMessage": "Id tidak boleh kosong",
+		})
+		return
+	}
+	id, err := strconv.Atoi(idText)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success":      false,
+			"errorMessage": "Id harus angka",
+		})
+		return
+	}
+	frm, err := frmsHandler.frmsUsecase.Get(id)
+	if err != nil {
+		appError := apperror.AppError{}
+		if errors.As(err, &appError) {
+			fmt.Printf("ServiceHandler.Create() 1 : %v ", err.Error())
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"success":      false,
+				"errorMessage": appError.Error(),
+			})
+			return
+		} else {
+
+			fmt.Printf("frmsHandler.frmsUsecase.Get() : %v ", err.Error())
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"success":      false,
+				"errorMessage": "Terjadi kesalahan ketika mengambil data service",
+			})
+			return
+		}
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    frm,
+	})
+}
+
 func NewFarmersHandler(srv *gin.Engine, frmsUsecase usecase.FarmersUsecase) FarmersHandler {
 	frmsHandler := &farmersHandlerImpl{
 		frmsUsecase: frmsUsecase,
 	}
+	srv.GET("/farmer/:id", frmsHandler.GetFarmerByIdHandler)
 	return frmsHandler
 }
--- usecase/farmers_usecase.go
+++ usecase/farmers_usecase.go
@@ -1,8 +1,14 @@
 package usecase
 
-import "live-code-farmer-ii/repo"
+import (
+	"fmt"
+	"live-code-farmer-ii/apperror"
+	"live-code-farmer-ii/model"
+	"live-code-farmer-ii/repo"
+)
 
 type FarmersUsecase interface {
+	Get(int) (*model.FarmersModel, error)
 }
 
 type farmersUsecaseImpl struct {
@@ -13,4 +19,18 @@ func NewFarmersUsecase(frmsRepo repo.FarmersRepo) FarmersUsecase {
 	return &farmersUsecaseImpl{
 		frmsRepo: frmsRepo,
 	}
+}
+
+func (frmsUsecase *farmersUsecaseImpl) Get(id int) (*model.FarmersModel, error) {
+	frm, err := frmsUsecase.frmsRepo.GetById(id)
+	if err != nil {
+		return nil, fmt.Errorf("frmsUsecase.frmsRepo.GetById() : %w", err)
+	}
+	if frm == nil {
+		return nil, apperror.AppError{
+			ErrorCode:    400,
+			ErrorMassage: fmt.Sprintf("data farmer dengan id :%d tidak ada", id),
+		}
+	}
+	return frm, nil
 }
